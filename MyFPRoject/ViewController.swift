48fe8ec8ee8d4c56607e21322d7929d2dc5f388f
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-04 21:06:58 +0300

    Initial Commit

---
+++ MyFPRoject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFPRoject
+//
+//  Created by Ömer Faruk Kılıçaslan on 4.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
